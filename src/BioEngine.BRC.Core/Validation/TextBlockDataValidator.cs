4333e93011040e686fc0cbfd6f7f545421982f81
George Drak (sonicgd) <sonicgd@gmail.com>
2020-11-18 22:38:55 +0500

    Validate block data instead of block

---
+++ src/BioEngine.BRC.Core/Validation/TextBlockDataValidator.cs
@@ -0,0 +1,15 @@
+﻿using BioEngine.BRC.Core.Entities.Blocks;
+using FluentValidation;
+using JetBrains.Annotations;
+
+namespace BioEngine.BRC.Core.Validation
+{
+    [UsedImplicitly]
+    public class TextBlockDataValidator : AbstractValidator<TextBlockData>
+    {
+        public TextBlockDataValidator()
+        {
+            RuleFor(p => p.Text).MinimumLength(250).WithMessage("Текст поста не должен быть меньше 250 символов.");
+        }
+    }
+}
